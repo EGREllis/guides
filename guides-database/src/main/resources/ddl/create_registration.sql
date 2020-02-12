c3e7e11f60732485475c592549e67c471763122e
egrellis <edwardellis.egre@gmail.com>
2020-02-12 17:58:41 +0000

    Removed permissions from registration

--- guides-database/src/main/resources/ddl/create_registration.sql
+++ guides-database/src/main/resources/ddl/create_registration.sql
@@ -2,6 +2,5 @@ CREATE TABLE guides.registration (
   registration_id INT NOT NULL,
   username VARCHAR(8) NOT NULL,
   password VARCHAR(16) NOT NULL,
-  permissions INT NOT NULL,
   PRIMARY KEY (registration_id)
 );
