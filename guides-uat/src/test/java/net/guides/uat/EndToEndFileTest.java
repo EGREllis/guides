806039dd2c3c5c541e426cf75373d3d7955c0658
egrellis <edwardellis.egre@gmail.com>
2020-03-10 16:57:14 +0000

    Added uat module

--- guides-uat/src/test/java/net/guides/uat/EndToEndFileTest.java
+++ guides-uat/src/test/java/net/guides/uat/EndToEndFileTest.java
@@ -1,4 +1,28 @@
 package net.guides.uat;
 
-public class EndToEndTest {
+import net.guides.data.DataAccessFacade;
+import net.guides.data.file.FileInMemoryDataAccessFacade;
+import net.guides.view.SwingView;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class EndToEndFileTest {
+    @Test
+    public void addClient() {
+        Properties properties = new Properties();
+        try (InputStream inputStream = ClassLoader.getSystemResourceAsStream("uat.properties")) {
+            properties.load(inputStream);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+        DataAccessFacade facade = new FileInMemoryDataAccessFacade(properties);
+        SwingView view = new SwingView(properties, facade);
+        facade.start();
+        view.start();
+
+
+    }
 }
